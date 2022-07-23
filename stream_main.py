8090393c978adf6ff3572d63f4b42448cf0ba192
liang185954 <109864168+liang185954@users.noreply.github.com>
2022-07-23 17:35:10 +0100

    Update stream_main.py

--- stream_main.py
+++ stream_main.py
@@ -3,13 +3,13 @@ import streamlit.components.v1 as components
 import pyvista
 import shutil
 
-file = st.file_uploader("Choose a file, vtk only")    
+file = st.file_uploader("Choose a file, VTK only")    
 if file is not None:
     # save the uploaded mesh file to disk
-    with open("new_mesh.vtk", "wb") as buffer:
+    with open("mesh.vtk", "wb") as buffer:
         shutil.copyfileobj(file, buffer)
         
-    mesh = pyvista.read('new_mesh.vtk')
+    mesh = pyvista.read('mesh.vtk')
     clipped = mesh.clip('y', invert=False)
     pl = pyvista.Plotter(shape=(1,2))
     _ = pl.add_mesh(mesh, style='wireframe', color='blue', label='Input')
@@ -17,7 +17,7 @@ if file is not None:
     pl.subplot(0,1)
     _ = pl.add_mesh(mesh, show_edges=True)
     pl.export_html('pyvista.html')  # doctest:+SKIP
-    option=st.sidebar.radio('Pyvista',('On','Off'))
+    option=st.sidebar.radio('Show Meshes',('On','Off'))
     if option=='On':
         HtmlFile = open("pyvista.html", 'r', encoding='utf-8')
         source_code = HtmlFile.read() 
