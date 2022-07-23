b5fa7deae0cd23d4d96827f1a8f24462ce9acda0
liang185954 <109864168+liang185954@users.noreply.github.com>
2022-07-23 16:00:09 +0100

    Update stream_main.py

--- stream_main.py
+++ stream_main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 st.title('Uber pickups in NYC')
-uploaded_file = st.file_uploader("Choose a file")
+uploaded_file = st.file_uploader("Choose a Mesh file")
 if uploaded_file is not None:
      # To read file as bytes:
      bytes_data = uploaded_file.getvalue()
