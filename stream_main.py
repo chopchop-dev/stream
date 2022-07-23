011b6f6db6c487c0d51714b87a2a12d8f584bc2c
liang185954 <109864168+liang185954@users.noreply.github.com>
2022-07-23 17:33:21 +0100

    Update stream_main.py

--- stream_main.py
+++ stream_main.py
@@ -3,15 +3,16 @@ import streamlit.components.v1 as components
 import pyvista
 import shutil
 
-file = st.file_uploader("Choose a file")    
+file = st.file_uploader("Choose a file, vtk only")    
 if file is not None:
-    # save the uploaded file to disk
+    # save the uploaded mesh file to disk
     with open("new_mesh.vtk", "wb") as buffer:
         shutil.copyfileobj(file, buffer)
         
     mesh = pyvista.read('new_mesh.vtk')
     clipped = mesh.clip('y', invert=False)
     pl = pyvista.Plotter(shape=(1,2))
+    _ = pl.add_mesh(mesh, style='wireframe', color='blue', label='Input')
     _ = pl.add_mesh(clipped, label='Clipped',show_edges=True)
     pl.subplot(0,1)
     _ = pl.add_mesh(mesh, show_edges=True)
