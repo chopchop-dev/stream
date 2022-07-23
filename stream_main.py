cec22580747a9305e18325348057435703a9283f
liang185954 <109864168+liang185954@users.noreply.github.com>
2022-07-23 17:25:09 +0100

    Update stream_main.py

--- stream_main.py
+++ stream_main.py
@@ -1,35 +1,9 @@
-from typing import Dict
 import streamlit as st
 
-@st.cache(allow_output_mutation=True)
-def get_static_store() -> Dict:
-    """This dictionary is initialized once and can be used to store the files uploaded"""
-    return {}
-
-def main():
-    """Run this function to run the app"""
-    static_store = get_static_store()
-
-    st.info(__doc__)
-    result = st.file_uploader("Upload", type="pdf")
-    if result:
-        # Process you file here
-        value = result.getvalue()
-
-        # And add it to the static_store if not already in
-        if not value in static_store.values():
-            static_store[result] = value
-    else:
-        static_store.clear()  # Hack to clear list if the user clears the cache and reloads the page
-        st.info("Upload one or more `.pdf` files.")
-
-    if st.button("Clear file list"):
-        static_store.clear()
-    if st.checkbox("Show file list?", True):
-        st.write(list(static_store.keys()))
-    if st.checkbox("Show content of files?"):
-        for value in static_store.values():
-            st.code(value)
-
-
-main()
+file = st.file_uploader("Choose a file")    
+if file is not None:
+    # save the uploaded file to disk
+    with open("new_mesh.vtk", "wb") as buffer:
+        shutil.copyfileobj(file, buffer)
+    tlog2 = log_analyzer.TrajectoryLog("new_mesh.vtk")
+    
