d2c3e0348bfc7c1f5846b4428c239e3ddbcc10ae
liang185954 <109864168+liang185954@users.noreply.github.com>
2022-07-23 16:40:42 +0100

    Update stream_main.py

--- stream_main.py
+++ stream_main.py
@@ -4,7 +4,7 @@ import pyvista
 import pandas as pd
 import numpy as np
 st.title('Uber pickups in NYC')
-uploaded_file = st.file_uploader("Choose a Mesh file")
+uploaded_file = st.file_uploader("Upload Mesh, vtk only",type=["vtk"])
 if uploaded_file is not None:
      # To read file as bytes:
      bytes_data = uploaded_file.getvalue()
