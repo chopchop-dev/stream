49470e59c27ae4f75aba1a5697a914d523f1e019
liang185954 <109864168+liang185954@users.noreply.github.com>
2022-07-23 17:30:43 +0100

    Update stream_main.py

--- stream_main.py
+++ stream_main.py
@@ -8,5 +8,17 @@ if file is not None:
     # save the uploaded file to disk
     with open("new_mesh.vtk", "wb") as buffer:
         shutil.copyfileobj(file, buffer)
+        
     mesh = pyvista.read('new_mesh.vtk')
-    
+    clipped = mesh.clip('y', invert=False)
+    pl = pyvista.Plotter(shape=(1,2))
+    _ = pl.add_mesh(clipped, label='Clipped',show_edges=True)
+    pl.subplot(0,1)
+    _ = pl.add_mesh(mesh, show_edges=True)
+    pl.export_html('pyvista.html')  # doctest:+SKIP
+    option=st.sidebar.radio('Pyvista',('On','Off'))
+    if option=='On':
+        HtmlFile = open("pyvista.html", 'r', encoding='utf-8')
+        source_code = HtmlFile.read() 
+        components.html(source_code, height = 500,width=500)
+  
