4bdd1b05cc3c6ca508503af1664013fa9354b118
liang185954 <109864168+liang185954@users.noreply.github.com>
2022-07-23 17:27:59 +0100

    Update stream_main.py

--- stream_main.py
+++ stream_main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import streamlit.components.v1 as components
+import pyvista
 import shutil
 
 file = st.file_uploader("Choose a file")    
@@ -6,5 +8,5 @@ if file is not None:
     # save the uploaded file to disk
     with open("new_mesh.vtk", "wb") as buffer:
         shutil.copyfileobj(file, buffer)
-    tlog2 = log_analyzer.TrajectoryLog("new_mesh.vtk")
+    mesh = pyvista.read('new_mesh.vtk')
     
