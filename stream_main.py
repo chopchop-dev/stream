577d4d61bc07e24d678bb9039ddff6accf448fb7
liang185954 <109864168+liang185954@users.noreply.github.com>
2022-07-23 17:12:57 +0100

    Update stream_main.py

--- stream_main.py
+++ stream_main.py
@@ -1,32 +1,35 @@
+from typing import Dict
 import streamlit as st
-import streamlit.components.v1 as components
-import pyvista
-import pandas as pd
-import numpy as np
-st.title('Uber pickups in NYC')
+
+@st.cache(allow_output_mutation=True)
+def get_static_store() -> Dict:
+    """This dictionary is initialized once and can be used to store the files uploaded"""
+    return {}
+
+def main():
+    """Run this function to run the app"""
+    static_store = get_static_store()
+
+    st.info(__doc__)
+    result = st.file_uploader("Upload", type="py")
+    if result:
+        # Process you file here
+        value = result.getvalue()
+
+        # And add it to the static_store if not already in
+        if not value in static_store.values():
+            static_store[result] = value
+    else:
+        static_store.clear()  # Hack to clear list if the user clears the cache and reloads the page
+        st.info("Upload one or more `.py` files.")
+
+    if st.button("Clear file list"):
+        static_store.clear()
+    if st.checkbox("Show file list?", True):
+        st.write(list(static_store.keys()))
+    if st.checkbox("Show content of files?"):
+        for value in static_store.values():
+            st.code(value)
 
 
-uploaded_file = st.file_uploader("Upload Mesh, vtk only",type=["vtk"])
-if uploaded_file is not None:
-     # To read file as bytes:
-     bytes_data = uploaded_file.getvalue()
-     file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type}
-     save_uploadedfile(bytes_data)
-     mesh = pyvista.read('new_mesh.vtk')
-     clipped = mesh.clip('y', invert=False)
-     pl = pyvista.Plotter(shape=(1,2))
-     _ = pl.add_mesh(clipped, label='Clipped',show_edges=True)
-     pl.subplot(0,1)
-     _ = pl.add_mesh(mesh, show_edges=True)
-     pl.export_html('pyvista.html')  # doctest:+SKIP
-     option=st.sidebar.radio('Pyvista',('On','Off'))
-     if option=='On':
-       HtmlFile = open("pyvista.html", 'r', encoding='utf-8')
-       source_code = HtmlFile.read() 
-       components.html(source_code, height = 500,width=500)
-
-
-def save_uploadedfile(uploadedfile):
-     with open(os.path.join("tempDir",uploadedfile.name),"wb") as f:
-         f.write(uploadedfile.getbuffer())
-     return st.success("Saved File:{} to tempDir".format(uploadedfile.name))
+main()
