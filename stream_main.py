e4f3f3ecc5eca106ca281e9177f9ff3b3291b640
liang185954 <109864168+liang185954@users.noreply.github.com>
2022-07-28 22:14:51 +0100

    Update stream_main.py

--- stream_main.py
+++ stream_main.py
@@ -8,7 +8,7 @@ import yaml
 import streamlit_authenticator as stauth
 
 with open('config.yaml') as file:
-    config = yaml.SafeLoader(file)
+    config = yaml.load(file)
     print(config)
 authenticator = stauth.Authenticate(
     config['cookie']['name'],
