d5cb604352c81f464cc7871ef1e45d65c94d6827
liang185954 <109864168+liang185954@users.noreply.github.com>
2022-07-28 18:16:33 +0100

    Update stream_main.py

--- stream_main.py
+++ stream_main.py
@@ -6,18 +6,29 @@ import pickle
 from pathlib import Path
 import streamlit_authenticator as stauth
 
-with open('../config.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
 
-authenticator = Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized']
-)
+st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
 
-name, authentication_status, username = authenticator.login('Login', 'main')
+# --- USER AUTHENTICATION ---
+names = ["Peter Parker", "Rebecca Miller"]
+usernames = ["pparker", "rmiller"]
+
+# load hashed passwords
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("rb") as file:
+    hashed_passwords = pickle.load(file)
+
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
+    "sales_dashboard", "abcdef", cookie_expiry_days=30)
+
+name, authentication_status, username = authenticator.login("Login", "main")
+
+if authentication_status == False:
+    st.error("Username/password is incorrect")
+
+if authentication_status == None:
+    st.warning("Please enter your username and password")
+
 
 file = st.file_uploader("Choose a file, VTK only")    
 if file is not None:
