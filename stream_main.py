7f7380dcc95ce615b47b0e9c05dde8534fdcfb64
liang185954 <109864168+liang185954@users.noreply.github.com>
2022-07-28 18:00:25 +0100

    Update stream_main.py

--- stream_main.py
+++ stream_main.py
@@ -2,6 +2,20 @@ import streamlit as st
 import streamlit.components.v1 as components
 import pyvista
 import shutil
+import streamlit_authenticator as stauth
+
+with open('../config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+name, authentication_status, username = authenticator.login('Login', 'main')
 
 file = st.file_uploader("Choose a file, VTK only")    
 if file is not None:
