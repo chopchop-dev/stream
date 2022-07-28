28ca5c3da0e48a234b70343f0316a2715186d0d7
liang185954 <109864168+liang185954@users.noreply.github.com>
2022-07-28 21:26:08 +0100

    Update stream_main.py

--- stream_main.py
+++ stream_main.py
@@ -6,28 +6,15 @@ import pickle
 from pathlib import Path
 import streamlit_authenticator as stauth
 
+with open('../config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
 
-st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
+authenticator = Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
 
-# --- USER AUTHENTICATION ---
-names = ["Peter Parker", "Rebecca Miller"]
-usernames = ["pparker", "rmiller"]
-
-# load hashed passwords
-file_path = Path(__file__).parent / "hashed_pw.pkl"
-with file_path.open("rb") as file:
-    hashed_passwords = pickle.load(file)
-
-authenticator = stauth.Authenticate(names, usernames, hashed_passwords)
-
-name, authentication_status, username = authenticator.login("Login", "main")
-
-if authentication_status == False:
-    st.error("Username/password is incorrect")
-
-if authentication_status == None:
-    st.warning("Please enter your username and password")
-
-
-if authentication_status:
-    st.warning('pass')
+name, authentication_status, username = authenticator.login('Login', 'main')
